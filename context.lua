b8019cdca855390dd5dcd12eebdb464c6234c698
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-25 16:15:29 -0500

    Load mods from lua context

--- context.lua
+++ context.lua
@@ -1,3 +1,5 @@
+print("------- begin lua ---------")
+
 print("CREATING LUA CONTEXT!")
 
 crafter = {
@@ -5,24 +7,35 @@ crafter = {
     }
 }
 
+current_loading_mod = ""
+
+crafter.directory = io.popen"cd":read'*l'
+
 print("LUA CONTEXT CREATED")
 
 print("INITIALIZING DEBUG FUNCTIONS")
 
-crafter.register_block = function(name, texture)
-    crafter.blocks[name] = {
-        name = name,
-        texture = texture
-    }
+crafter.register_block = function(table_data)
+    table_data.mod = current_loading_mod
+    crafter.blocks[table_data.name] = table_data
 end
 
-print("NED INITIALIZING DEBUG FUNCTIONS")
+print("END INITIALIZING DEBUG FUNCTIONS")
 
 print("TESTING DEBUG FUNCTIONS")
 
-crafter.register_block("dirt", "dirt.png")
-
-crafter.register_block("debug_alpha", "debug_alpha.png")
-
 
 print("ENDING TESTING DEBUG FUNCTIONS")
+
+
+
+print(crafter.directory)
+
+local f = io.popen("dir " .. crafter.directory .. "\\mods /b /ad")
+
+for mod in f:lines() do
+    current_loading_mod = mod
+    dofile(crafter.directory .. "\\mods\\" .. mod .. "\\main.lua")
+end
+
+ print("----- end lua --------- ")
