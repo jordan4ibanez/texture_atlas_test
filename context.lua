bb0a31153538723336e7a7d68d0797820ef21c15
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-02-24 21:25:35 -0500

    Create extremely basic register_block() in lua

--- context.lua
+++ context.lua
@@ -1,10 +1,28 @@
-print("hello there boi")
+print("CREATING LUA CONTEXT!")
 
 crafter = {
     blocks = {
-        dirt = {
-            name = "dirt",
-            texture = "test.png"
-        }
     }
 }
+
+print("LUA CONTEXT CREATED")
+
+print("INITIALIZING DEBUG FUNCTIONS")
+
+crafter.register_block = function(name, texture)
+    crafter.blocks[name] = {
+        name = name,
+        texture = texture
+    }
+end
+
+print("NED INITIALIZING DEBUG FUNCTIONS")
+
+print("TESTING DEBUG FUNCTIONS")
+
+crafter.register_block("dirt", "dirt.png")
+
+crafter.register_block("debug_alpha", "debug_alpha.png")
+
+
+print("ENDING TESTING DEBUG FUNCTIONS")
